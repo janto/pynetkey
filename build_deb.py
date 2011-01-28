6937e609478227fb4020a3a52a20814d48780ef3
Janto Dreijer <jantod@gmail.com>
2011-01-28 15:57:50 +0200

    first attempt at basic deb build

---
+++ build_deb.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+from __future__ import with_statement
+from __init__ import version
+import os
+
+def main():
+	data_dir = os.path.abspath(".")
+	base_dir = "/tmp/pynetkey"
+	deb_dir = os.path.join(base_dir, "DEBIAN")
+	install_dir = os.path.join(base_dir, "")
+	try:
+		os.makedirs(deb_dir)
+	except OSError:
+		pass
+	try:
+		os.makedirs(install_dir)
+	except OSError:
+		pass
+
+	control_text = """
+Package: pynetkey
+Version: %(version)s
+Section: web
+Priority: optional
+Architecture: all
+Essential: no
+Depends: python-eggtrayicon, python (>=2.6)
+Installed-Size: 128
+Maintainer: Janto Dreijer <jantod@gmail.com>
+Provides: pynetkey
+Description: GPL alternative to the official inetkey/sinetkey.
+	Its goals are to be more robust and provide some extra functionality.
+""" % dict(version=version)
+	with file(os.path.join(deb_dir, "control"), "w") as f:
+		f.write(control_text)
+
+	with file(os.path.join(deb_dir, "postinst"), "w") as f:
+		f.write("#!/bin/sh\n")
+	os.chmod(os.path.join(deb_dir, "postinst"), 0755) # make executable
+
+	with file(os.path.join(deb_dir, "prerm"), "w") as f:
+		f.write("#!/bin/sh\n")
+	os.chmod(os.path.join(deb_dir, "prerm"), 0755) # make executable
+
+	os.system("dpkg --build %s ." % base_dir)
+
+if __name__ == "__main__":
+	main()
