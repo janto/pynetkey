45579624be451152d4b92711574392d6ed167a79
Janto Dreijer <jantod@gmail.com>
2011-02-02 18:00:06 +0200

    deb install now adds a menu shortcut

--- build_deb.py
+++ build_deb.py
@@ -3,23 +3,44 @@
 from __future__ import with_statement
 from __init__ import version
 import os
+import shutil
 
 def main():
 	data_dir = os.path.abspath(".")
 	base_dir = "/tmp/pynetkey"
-	deb_dir = os.path.join(base_dir, "DEBIAN")
+	shutil.rmtree(base_dir)
+
 	install_dir = os.path.join(base_dir, "usr/share/pyshared/pynetkey")
-	try:
-		os.makedirs(deb_dir)
-	except OSError:
-		pass
 	try:
 		os.makedirs(install_dir)
 	except OSError:
 		pass
-
 	os.system("hg clone static-http://dip.sun.ac.za/~janto/pynetkey/repo %s" % install_dir)
 
+	shortcut_dir = os.path.join(base_dir, "usr/share/applications")
+	try:
+		os.makedirs(shortcut_dir)
+	except OSError:
+		pass
+	shortcut_text = """
+[Desktop Entry]
+Version=1.0
+Name=Pynetkey
+Comment=Inetkey in Python
+Exec=python /usr/share/pyshared/pynetkey/pynetkey.py
+Terminal=false
+Type=Application
+Icon=/usr/share/pyshared/pynetkey/icons/main.png
+Categories=Network;
+""".lstrip()
+	with file(os.path.join(shortcut_dir, "pynetkey.desktop"), "w") as f:
+		f.write(shortcut_text)
+
+	deb_dir = os.path.join(base_dir, "DEBIAN")
+	try:
+		os.makedirs(deb_dir)
+	except OSError:
+		pass
 	control_text = """
 Package: pynetkey
 Version: %(version)s
@@ -33,7 +54,7 @@ Maintainer: Janto Dreijer <jantod@gmail.com>
 Provides: pynetkey
 Description: Unofficial GPL alternative to inetkey/sinetkey.
 	Its goals are to be more robust and provide some extra functionality.
-""" % dict(version=version)
+""".lstrip() % dict(version=version)
 	with file(os.path.join(deb_dir, "control"), "w") as f:
 		f.write(control_text)
 
