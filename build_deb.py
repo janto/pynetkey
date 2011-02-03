2f43e3fbde983f0f3ac8f82b9c1c080b2e600feb
Janto Dreijer <jantod@gmail.com>
2011-02-03 15:08:15 +0200

    add copyright file to deb

--- build_deb.py
+++ build_deb.py
@@ -49,7 +49,7 @@ Priority: optional
 Architecture: all
 Essential: no
 Depends: python-eggtrayicon, python (>=2.6)
-Installed-Size: 128
+Installed-Size: 150
 Maintainer: Janto Dreijer <jantod@gmail.com>
 Provides: pynetkey
 Description: Unofficial GPL alternative to inetkey/sinetkey.
@@ -57,6 +57,37 @@ Description: Unofficial GPL alternative to inetkey/sinetkey.
 """.lstrip() % dict(version=version)
 	with file(os.path.join(deb_dir, "control"), "w") as f:
 		f.write(control_text)
+
+	copyright_text = """
+Upstream Author(s):
+
+    Janto Dreijer <jantod@gmail.com>
+
+Copyright:
+
+    Copyright (C) 2011 Janto Dreijer
+
+License:
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This package is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+On Debian systems, the complete text of the GNU General
+Public License version 3 can be found in `/usr/share/common-licenses/GPL-3'.
+
+""".lstrip()
+	with file(os.path.join(deb_dir, "copyright"), "w") as f:
+		f.write(copyright_text)
 
 	with file(os.path.join(deb_dir, "postinst"), "w") as f:
 		f.write("#!/bin/sh\n")
