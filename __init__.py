7754f51c29463e04fd07a4a340c657b3139eb4a3
Janto Dreijer <jantod@gmail.com>
2012-04-01 18:37:22 +0200

    update version

--- __init__.py
+++ __init__.py
@@ -2,9 +2,5 @@
 
 """
 the version number is shown in the title bar of the password dialog box.
-
-it is also sent with each http request in case IT one day decides to block a specific version
-this is especially important for the get_usage requests. because it's done every minute, the traffic might become significant if pynetkey's usage spreads.
-yeah, I'm paranoid.
 """
-version = "20120313"
+version = "20120401"
